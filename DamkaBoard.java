0e2df349e91a0578645d8745713e8830baa39421
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2023-12-30 01:37:54 +0200

    Damka da Ne

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,26 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int input = Integer.parseInt(args[0]);
+		int verticalCounter = 1;
+		int horizontalCounter = 0;
+	 	int evenChecker = 1;
+ 		while (horizontalCounter < input){
+			while (verticalCounter <= input){
+				System.out.print("* ");
+				verticalCounter++;
+			}
+			horizontalCounter++;
+			if (evenChecker % 2 == 0){
+			System.out.println("");
+			evenChecker++;
+			}else {
+				System.out.println("");
+				System.out.print(" ");
+			evenChecker++;}
+			verticalCounter = 1;
+		}
 	}
 }
+
+	
+
