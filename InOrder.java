6d5b3c7bbe0b3b85ed94c930bce1968f1cc11a02
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2023-12-29 17:19:51 +0200

    The randoms are in order

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,12 @@
-/**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
- */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		int random1 = (int)(Math.random() * 10);
+		int random2 = (int)(Math.random() * 10);
+		System.out.print(random1 + " ");
+		while (random2 > random1){
+			System.out.print(random2 + " ");
+			random1 = random2;
+			random2 = (int)(Math.random() * 10);
+		}
 	}
 }
