972df57807dd65586f3e5055472535be1dc04a98
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2023-12-30 00:18:51 +0200

    DIvisors

--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,27 @@
-/**
- *  Gets a command-line argument (int), and chekcs if the given number is perfect.
- */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int input = Integer.parseInt(args[0]);
+		int checker = 0;
+		int start = 1;
+		int notLastDivisor = 0;
+		while (start < input){
+			int divisor = input % start;
+			if (divisor == 0) {
+				notLastDivisor = start;
+				checker = checker + start; 
+				start++;}
+			else {start++;}
+			} 
+		if (checker == input){
+			System.out.print(input + " is a perfect number since " + input + " = 1 + ");
+			start = 2;
+			while (start < input){
+				if (start < input && input%start == 0 && start != notLastDivisor){
+					System.out.print(start + " + ");
+					start++;
+				}else if (start == notLastDivisor) {System.out.print(start); start++;}
+				else{start++;}
+			}
+		} else {System.out.print(input + " is not a perfect number");}
 	}
 }
